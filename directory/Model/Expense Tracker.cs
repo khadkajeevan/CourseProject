3856d955f6ab410c57cbaa7394d6847ff4191394
Jeevan Khadka <jeevan.khadka1@outlook.com>
2019-08-31 19:13:42 -0400

    added EF for database

---
+++ directory/Model/Expense Tracker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Model
+{
+    public class SurveyContext : DbContext
+    {
+        public DbSet<Registration> Registration { get; set; }
+        public DbSet<Login> Login { get; set; }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<Balance> Balance { get; set; }
+        public DbSet<Goal> Goal { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Database=ExpenseTracker");
+        }
+    }
+
+    public class Registration
+    {
+        [Key]
+        public int PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }       
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Login
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Expense
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public int Date { get; set; }
+        public int ExpenseTotal { get; set; }
+        public string UserId { get; set; }
+        
+    }
+
+    public class Balance
+    {
+        [Key]
+        public int Income { get; set; }
+        public string UserId { get; set; }
+        public int BankBalance { get; set; }        
+        public int ExpenseTotal { get; set; }
+    }
+
+    public class Goal
+    {
+        [Key]
+        public int TargetAmount { get; set; }
+        public string UserId { get; set; }        
+        public int ExpenseTotal { get; set; }
+        public int RemainingExpense { get; set; }
+    }
+
+    
+}
---
+++ directory/Model/Migrations/20190831193606_InitialCreate.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Model.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Balance",
+                columns: table => new
+                {
+                    Income = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(nullable: true),
+                    BankBalance = table.Column<int>(nullable: false),
+                    ExpenseTotal = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Balance", x => x.Income);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expense",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<int>(nullable: false),
+                    ExpenseTotal = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expense", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Goal",
+                columns: table => new
+                {
+                    TargetAmount = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(nullable: true),
+                    ExpenseTotal = table.Column<int>(nullable: false),
+                    RemainingExpense = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goal", x => x.TargetAmount);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Login",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Login", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registration",
+                columns: table => new
+                {
+                    PhoneNumber = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registration", x => x.PhoneNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Balance");
+
+            migrationBuilder.DropTable(
+                name: "Expense");
+
+            migrationBuilder.DropTable(
+                name: "Goal");
+
+            migrationBuilder.DropTable(
+                name: "Login");
+
+            migrationBuilder.DropTable(
+                name: "Registration");
+        }
+    }
+}
