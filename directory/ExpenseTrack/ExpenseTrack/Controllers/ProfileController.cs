3557e0feccc165cd0cda2cae82fe4455eebc6790
Jeevan Khadka <jeevan.khadka1@outlook.com>
2019-11-04 15:10:18 -0500

    Implementing ASP.NET

---
+++ directory/ExpenseTrack/ExpenseTrack/Controllers/ProfileController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseTrack.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrack.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult BankBalance()
+        {
+            return View();
+        }
+
+        public IActionResult ExpenseAmount()
+        {
+            return View();
+        }
+        public IActionResult RemainingExpense()
+        {
+            return View();
+        }
+        public IActionResult Expense()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        //// GET: Profile
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //// GET: Profile/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: Profile/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Profile/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Profile/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Profile/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Profile/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Profile/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ directory/ExpenseTrack/ExpenseTrack/Models/ExpenseTrackDB.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTrack.Models
+{
+    public class ExpenseTrackDB : IdentityDbContext<IdentityUser>  //dbcontext previous
+    {
+       
+        public DbSet<BankBalance> BankBalance { get; set; }
+        public DbSet<TargetedExpenses> TargetedExpenses { get; set; }
+        public DbSet<ExpensesCategory> ExpensesCategory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Database=ExpenseTracker");
+        }
+    }
+
+    public class ExpensesCategory
+    {
+    
+        public int ExpensesCategoryId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalExpenses { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class BankBalance
+    {
+        [Key]
+        public int  { get; set; }
+        public int Income { get; set; }
+        public string UserId { get; set; }
+        public int Balance { get; set; }
+        public int ExpenseTotal { get; set; }
+    }
+
+    public class TargetedExpenses
+    {
+       
+        public int TargetAmount { get; set; }
+        public string UserId { get; set; }
+        public int ExpenseTotal { get; set; }
+        public int RemainingExpense { get; set; }
+    }
+
+}
